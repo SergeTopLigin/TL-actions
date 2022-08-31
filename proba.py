a928a0b2569ad8a6587ed2d039002f3aec5de527
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2022-08-31 20:23:58 +0300

    Add files via upload

---
+++ proba.py
@@ -0,0 +1,11 @@
+# help(newline)
+
+# import time
+# d = time.localtime()
+# date_fname = str(d.tm_year) + '-' + str(d.tm_mon)  + '-' + str(d.tm_mday)  + '_' + str(d.tm_hour)  + '-' + str(d.tm_min)  + '-' + str(d.tm_sec)
+# fname = 'answer ' + date_fname
+# open("C:\\Users\\Серж\\Desktop\\TopLiga\\!new realities\\TL_work\\API interaction\\answers\\"+fname+".txt", 'w')
+
+l = ['s', 'd', 'f']
+for q in l:
+    print(q)
