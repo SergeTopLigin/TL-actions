ee25b0bc685303206be0d7b43cc5eb8a57926846
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2022-09-02 13:50:06 +0300

    Update proba.py

--- proba.py
+++ proba.py
@@ -1,2 +1,8 @@
 import os
 print(os.path.abspath(__file__))
+
+#os.chdir(os.path.dirname(os.path.abspath(__file__)))
+
+f = open("file.txt", "w")
+f.write("Строка") # Записываем строку в файл
+f. close ()
