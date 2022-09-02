0948b26f1501a2a818064c662a0b35e7daa03f6b
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2022-09-02 14:01:26 +0300

    Update proba.py

--- proba.py
+++ proba.py
@@ -1,6 +1,7 @@
 import os
 print(os.path.abspath(__file__))
 
+# Чтобы поиск файла всегда производился в каталоге с исполняемым файлом, необходимо этот каталог сделать текущим с помощью
 #os.chdir(os.path.dirname(os.path.abspath(__file__)))
 
 f = open("file.txt", "w")
