dddd8767b4d3dd2c816f71addfeab70aa427665f
SergeTopLigin <94070714+SergeTopLigin@users.noreply.github.com>
2023-12-03 21:18:04 +0300

    Add files via upload

---
+++ PY_GH.py
@@ -0,0 +1,15 @@
+from github import Github
+
+# Authentication is defined via github.Auth
+from github import Auth
+
+CodingToken = "2g2h2p2_2U2e2q2m212I2C2H232L2Z2J2C2h2f2p2q2s2S2z2S272Z2P2y2J2e2n2J2t202X2i2R2I2N2"
+
+# using an access token
+auth = Auth.Token("secrets.PY_GH")
+
+# Public Web Github
+g = Github(auth=auth)
+
+repo = g.get_repo("SergeTopLigin/TL-actions")
+repo.create_file("test.txt", "test", "test", branch="main")
